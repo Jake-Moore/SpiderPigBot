9c7e3b60108a130f7627fc1edfe600b2b4ac539c
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2020-05-14 18:53:27 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,15 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (!message.content.startsWith("??") || message.author.bot) return;
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")
     const args = message.content.slice(2).split(/ +/);
     const command = args.shift().toLowerCase();
     if (command == "dm"){
         if (args[0] == "everyone"){
             message.channel.send("will send to all"); 
+        }else{
+            var dmTo = client.users.get("name", arg[0]); 
+            dmTo.send("test");
         }
     }
 });
